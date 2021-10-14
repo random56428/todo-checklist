936d76237ac40ad396810381ec78db6d53e29604
jiajun03 <jiajun03@student.ubc.ca>
2021-10-14 15:24:23 -0700

    Added documentation for main method

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,8 @@ package ui;
 
 // Main class to create and start the user interface
 public class Main {
+
+    //EFFECTS: creates a new to-do list ui console application
     public static void main(String[] args) {
         new ToDoApp();
     }
